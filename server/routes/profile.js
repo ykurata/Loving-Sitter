d4568ae30a2310477428d30d05dd2e05916a2065
romaldowoho <roma.ushpayev@gmail.com>
2019-10-16 13:09:27 -0700

    cleaned up profile controllers

--- server/routes/profile.js
+++ server/routes/profile.js
@@ -7,6 +7,6 @@ var profile_controller = require("../controllers/profileController");
 router.post("/create", authenticate, profile_controller.createProfile);
 router.get("/get", authenticate, profile_controller.getAllProfiles);
 router.get("/get/:id", authenticate, profile_controller.getProfile);
-router.put("/update/:id", authenticate, profile_controller.profileUpdatePost);
+router.put("/update/:id", authenticate, profile_controller.updateProfile);
 
 module.exports = router;
