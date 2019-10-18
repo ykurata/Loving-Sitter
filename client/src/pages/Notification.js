06e9b634888ecb3ff829b2ba8e78edc64074ddf7
ykurata <yasukokurata@gmail.com>
2019-10-18 10:30:33 -0400

    created notification card

--- client/src/pages/Notification.js
+++ client/src/pages/Notification.js
@@ -4,6 +4,7 @@ import { makeStyles } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
 import Avatar from '@material-ui/core/Avatar';
 import Typography from '@material-ui/core/Typography';
+import ButtonBase from '@material-ui/core/ButtonBase';
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -12,14 +13,23 @@ const useStyles = makeStyles(theme => ({
     padding: theme.spacing(0, 3),
   },
   paper: {
-    maxWidth: 400,
-    margin: `${theme.spacing(1)}px auto`,
+    maxWidth: 450,
+    margin: `${theme.spacing(3)}px auto`,
     padding: theme.spacing(2),
+  },
+  image: {
+    width: 70,
+    height: 80,
+  },
+  img: {
+    margin: 'auto',
+    display: 'block',
+    maxWidth: '100%',
+    maxHeight: '100%',
+
   },
 }));
 
-const message = `Truncation should be conditionally applicable on this long line of text
- as this is a much longer line than what the container can support. `;
 
 export default function AutoGridNoWrap() {
   const classes = useStyles();
@@ -29,10 +39,27 @@ export default function AutoGridNoWrap() {
       <Paper className={classes.paper}>
         <Grid container wrap="nowrap" spacing={2}>
           <Grid item>
-            <Avatar>W</Avatar>
+            <ButtonBase className={classes.image}>
+              <img  className={classes.img} alt="complex"  src={require("../images/1a350ede83e5c0c4b87586c0d4bad0f66b86da37.png")} />
+            </ButtonBase>
           </Grid>
           <Grid item xs zeroMinWidth>
-            <Typography noWrap>{message}</Typography>
+            <Typography variant='subtitle1'>Marry has requested your service for 2 hours</Typography>
+            <Typography variant="caption" color="textSecondary">Dog Sitting</Typography>
+            <Typography variant="subtitle1">18/10/2019</Typography>
+          </Grid>
+        </Grid>
+
+        <Grid container wrap="nowrap" spacing={2}>
+          <Grid item>
+            <ButtonBase className={classes.image}>
+              <img className={classes.img} alt="complex" src={require("../images/4a9e3a82f8e96e82eb129a672414842b2efb5ab0.png")} />
+            </ButtonBase>
+          </Grid>
+          <Grid item xs zeroMinWidth>
+            <Typography variant='subtitle1'>Yasuko has requested your service for 5 hours</Typography>
+            <Typography variant="caption" color="textSecondary">Dog Sitting</Typography>
+            <Typography variant="subtitle1">25/10/2019</Typography>
           </Grid>
         </Grid>
       </Paper>
