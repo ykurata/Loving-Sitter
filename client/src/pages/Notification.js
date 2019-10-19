986589cb44a359dfab7b258c2aecc05f87cf6016
ykurata <yasukokurata@gmail.com>
2019-10-19 12:58:50 -0400

    cleaned code

--- client/src/pages/Notification.js
+++ client/src/pages/Notification.js
@@ -1,8 +1,6 @@
 import React from 'react';
-import Paper from '@material-ui/core/Paper';
 import Card from '@material-ui/core/Card';
 import List from '@material-ui/core/List';
-import ListItem from '@material-ui/core/ListItem';
 import Divider from '@material-ui/core/Divider';
 import { makeStyles } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
