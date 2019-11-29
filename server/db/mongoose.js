625ae12c6d2b46595320d8962187f9de20cecde8
ykurata <yasukokurata@gmail.com>
2019-11-29 09:29:08 -0500

    add useUnifiedTopology into mongoose.js

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -5,6 +5,7 @@ import config from "./../config";
 mongoose.set("useNewUrlParser", true);
 mongoose.set("useFindAndModify", false);
 mongoose.set("useCreateIndex", true);
+mongoose.set("useUnifiedTopology", true );
 
 mongoose.set("debug", true);
 
