68b0cfed6cd6d410bf17759bf4a6c3d6351fecb4
ykurata <yasukokurata@gmail.com>
2019-12-01 12:24:30 -0500

    display error message from backend in login page

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,76 +1,52 @@
 import React, { Component } from "react";
+import { withRouter } from "react-router-dom";
 import axios from "axios";
 import jwt_decode from "jwt-decode";
+
 import TextField from "@material-ui/core/TextField";
 import Grid from "@material-ui/core/Grid";
-import "../App.scss";
 import Button from "@material-ui/core/Button";
+import "../App.scss";
 
-import { withRouter } from "react-router-dom";
-
-const initalState = {
-  email: "",
-  emailError: "",
-  password: "",
-  passwordError: "",
-  errors: ""
-};
 
 class LoginPage extends Component {
-  state = initalState;
-
-  handleEmailChange = event => {
-    this.setState({ email: event.target.value });
-  };
-
-  handlePasswordChange = event => {
-    this.setState({ password: event.target.value });
-  };
-
-  validate = () => {
-    let emailError = "";
-    var re = /[^@]+@[^.]+..+/;
-    var test = re.test(this.state.email);
-    if (!test === true) {
-      emailError = "Invalid email";
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: "",
+      password: "",
+      error: "",
+      validationErrors: []
     }
+  }
+  
+  // Update user input
+  onChange = e => {
+    this.setState({ [e.target.name]: e.target.value });
+  }
 
-    if (emailError) {
-      this.setState({ emailError });
-      return false;
-    }
-
-    this.setState({ emailError: undefined });
-    return true;
-  };
-
-  handleSubmit = event => {
-    event.preventDefault();
-    const isValid = this.validate();
-
-    if (isValid) {
-      const { email, password } = this.state;
-
-      const data = {
-        email: email,
-        password: password
-      };
-      axios
-        .post("/users/login", data)
-        .then(res => {
-          const { token } = res.data;
-          const decoded = jwt_decode(token);
-          localStorage.setItem("jwtToken", token);
-          localStorage.setItem("userId", decoded.id);
-          console.log("successfully logged in ");
-          this.props.history.push("/profile");
-        })
-        .catch(err => {
-          this.setState({
-            errors: err.response.data.error // Error messages from backend
-          });
+  handleSubmit = e => {
+    e.preventDefault();
+    const { email, password } = this.state;
+    const data = {
+      email: email,
+      password: password
+    };
+    axios.post("/users/login", data)
+      .then(res => {
+        const { token } = res.data;
+        const decoded = jwt_decode(token);
+        localStorage.setItem("jwtToken", token);
+        localStorage.setItem("userId", decoded.id);
+        console.log("successfully logged in ");
+        this.props.history.push("/profile");
+      })
+      .catch(err => {
+        this.setState({
+          validationErrors: err.response.data, // Error messages from backend
+          error: err.response.data.error
         });
-    }
+      });  
   };
 
   render() {
@@ -86,14 +62,9 @@ class LoginPage extends Component {
                     <Grid item xs={12}>
                       <h1 className="center">LogIn</h1>
                     </Grid>
-                    {this.state.errors ? (
-                      <Grid
-                        item
-                        xs={12}
-                        className="pb-0 pt-0"
-                        style={{ color: "red" }}
-                      >
-                        <p className="mb-0 mt-0">{this.state.errors}</p>
+                    {this.state.error ? (
+                      <Grid item xs={12} className="pb-0 pt-0" style={{ color: "red" }}>
+                        <p>{this.state.error}</p>
                       </Grid>
                     ) : null}
                     <Grid item xs={12} className="pb-0 pt-0">
@@ -108,12 +79,14 @@ class LoginPage extends Component {
                         variant="outlined"
                         type="email"
                         value={this.state.email}
-                        onChange={this.handleEmailChange}
+                        onChange={this.onChange}
                         fullWidth
                       />
-                      <div style={{ color: "red" }}>
-                        {this.state.emailError}
-                      </div>
+                      {this.state.validationErrors ? 
+                        <div style={{ color: "red" }}>
+                          {this.state.validationErrors.email}
+                        </div>
+                      : null}
                     </Grid>
                     <Grid item xs={12} className="pb-0 pt-0">
                       <p className="mb-0 mt-0">PASSWORD</p>
@@ -127,9 +100,14 @@ class LoginPage extends Component {
                         variant="outlined"
                         type="password"
                         value={this.state.password}
-                        onChange={this.handlePasswordChange}
+                        onChange={this.onChange}
                         fullWidth
                       />
+                      {this.state.validationErrors ? 
+                        <div style={{ color: "red" }}>
+                          {this.state.validationErrors.password}
+                        </div>
+                      : null}
                     </Grid>
                     <Grid item xs={2}></Grid>
                     <Grid item xs={8} className="center">
