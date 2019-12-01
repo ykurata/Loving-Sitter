30722c0379372513f4316b6b89685dd10087e799
ykurata <yasukokurata@gmail.com>
2019-12-01 18:14:23 -0500

    update sign up page

--- client/src/pages/Signup.js
+++ client/src/pages/Signup.js
@@ -2,15 +2,35 @@ import React, { Component } from "react";
 import axios from "axios";
 import jwt_decode from "jwt-decode";
 
-import TextField from "@material-ui/core/TextField";
-import Grid from "@material-ui/core/Grid";
-import { Snackbar, IconButton } from "@material-ui/core";
-import Button from "@material-ui/core/Button";
-import "../App.scss";
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import TextField from '@material-ui/core/TextField';
+import Link from '@material-ui/core/Link';
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
+import { withStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
 
-export const snackbarmsg = "";
+const SignUpStyles = theme => ({
+  paper: {
+    marginTop: theme.spacing(8),
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  label: {
+    margin: theme.spacing(1, 1, 1, 0),
+  },
+  form: {
+    width: '100%', // Fix IE 11 issue.
+    marginTop: theme.spacing(1),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
+});
 
-class SignUpPage extends Component {
+class SignUp extends Component {
   constructor(props) {
     super(props);
     this.state = {
@@ -19,7 +39,6 @@ class SignUpPage extends Component {
       pasword: "",
       confirmPassword: "",
       validationErrors: [],
-      snackbaropen: false,
     }
   }
 
@@ -55,170 +74,104 @@ class SignUpPage extends Component {
     });
   };
 
-  snackbarClose = e => {
-    this.setState({ snackbaropen: false });
-  };
-
-  enableEdit = e => {
-    this.setState({ disabled: false });
-  };
-
   render() {
+    const { classes } = this.props;
     return (
-      <div>
-        <Snackbar
-          anchorOrigin={{
-            vertical: "bottom",
-            horizontal: "left"
-          }}
-          open={this.state.snackbaropen}
-          autoHideDuration={3000}
-          onClose={this.snackbarClose}
-          message={<span id="message-id">{this.state.snackbarmsg}</span>}
-          action={[
-            <IconButton
-              key="close"
-              arial-label="Close"
-              color="inherit"
-              onClick={this.snackbarClose}
-            ></IconButton>
-          ]}
-        />
-        <Grid container spacing={3}>
-          <Grid item xs={2}></Grid>
-          <Grid item xs={8}>
-            <div className="container">
-              <div className="infoBox ">
-                <form onSubmit={this.handleSubmit}>
-                  <Grid container spacing={3}>
-                    <Grid item xs={12}>
-                      <h1 className="center">Sign Up</h1>
-                    </Grid>
-
-                    {/* {
-                      (this.state.errors)
-                      ? <Grid item xs={12} className="pb-0 pt-0" style={{ color: "red" }}>
-                          <p className="mb-0 mt-0">{this.state.errors}</p>
-                        </Grid>
-                      : null
-                    } */}
-
-                    <Grid item xs={12} className="pb-0 pt-0">
-                      <p className="mb-0 mt-0">NAME</p>
-                    </Grid>
-                    <Grid item xs={12}>
-                      <TextField
-                        name="name"
-                        id="outlined-name"
-                        placeholder="Name"
-                        margin="normal"
-                        variant="outlined"
-                        type="text"
-                        value={this.state.name}
-                        onChange={this.onChange}
-                        fullWidth
-                      />
-                      {this.state.validationErrors ? 
-                        <div style={{ color: "red" }}>
-                          {this.state.validationErrors.name}
-                        </div>
-                      : null}
-                    </Grid>
-
-                    <Grid item xs={12} className="pb-0 pt-0">
-                      <p className="mb-0 mt-0">EMAIL ADDRESS</p>
-                    </Grid>
-                    <Grid item xs={12}>
-                      <TextField
-                        name="email"
-                        id="outlined-email"
-                        placeholder="Email"
-                        margin="normal"
-                        variant="outlined"
-                        type="email"
-                        value={this.state.email}
-                        onChange={this.onChange}
-                        fullWidth
-                      />
-                      {this.state.validationErrors ? 
-                        <div style={{ color: "red" }}>
-                          {this.state.validationErrors.email}
-                        </div>
-                      : null}
-                    </Grid>
-
-                    <Grid item xs={12} className="pb-0 pt-0">
-                      <p className="mb-0 mt-0">PASSWORD</p>
-                    </Grid>
-                    <Grid item xs={12}>
-                      <TextField
-                        name="password"
-                        id="outlined-password"
-                        placeholder="Password"
-                        margin="normal"
-                        variant="outlined"
-                        type="password"
-                        value={this.state.password}
-                        onChange={this.onChange}
-                        fullWidth
-                      />
-                      {this.state.validationErrors ? 
-                        <div style={{ color: "red" }}>
-                          {this.state.validationErrors.password}
-                        </div>
-                      : null}
-                    </Grid>
-
-                    <Grid item xs={12} className="pb-0 pt-0">
-                      <p className="mb-0 mt-0">CONFIRM PASSWORD</p>
-                    </Grid>
-                    <Grid item xs={12}>
-                      <TextField
-                        name="confirmPassword"
-                        id="outlined-confirmPassword"
-                        placeholder="Confrim Password"
-                        margin="normal"
-                        variant="outlined"
-                        type="password"
-                        value={this.state.confirmPassword}
-                        onChange={this.onChange}
-                        fullWidth
-                      />
-                      {this.state.validationErrors ? 
-                        <div style={{ color: "red" }}>
-                          {this.state.validationErrors.password2}
-                        </div>
-                      : null}
-                    </Grid>
-                    <Grid item xs={2}></Grid>
-                    <Grid item xs={8} className="center">
-                      <Button
-                        variant="contained"
-                        onClick={this.handleSubmit}
-                        fullWidth
-                        className="submit-button"
-                        size="large"
-                      >
-                        Sign Up
-                      </Button>
-                    </Grid>
-                    <Grid item xs={2}></Grid>
-
-                    <Grid item xs={12} className="center">
-                      <p>
-                        Already have an Account? <a href="../login">Login</a>
-                      </p>
-                    </Grid>
-                  </Grid>
-                </form>
-              </div>
+      <Container component="main" maxWidth="xs">
+      <CssBaseline />
+      <div className={classes.paper}>
+        <Typography component="h1" variant="h4">
+          Sign Up 
+        </Typography>
+        {this.state.error ? 
+          <div style={{ color: "red", marginTop: "10px" }}>
+            {this.state.error}
+          </div>
+        : null}
+        <form className={classes.form} onSubmit={this.handleSubmit}>
+          <Typography className={classes.label}>Name</Typography>
+          {this.state.validationErrors ? 
+            <div style={{ color: "red" }}>
+              {this.state.validationErrors.name}
             </div>
+          : null}
+          <TextField
+            variant="outlined"
+            margin="normal"
+            fullWidth
+            id="name"
+            name="name"
+            placeholder="Name"
+            onChange={this.onChange}
+          />
+          <Typography className={classes.label}>Email Address</Typography>
+          {this.state.validationErrors ? 
+            <div style={{ color: "red" }}>
+              {this.state.validationErrors.email}
+            </div>
+          : null}
+          <TextField
+            variant="outlined"
+            margin="normal"
+            fullWidth
+            id="email"
+            name="email"
+            placeholder="Email Address"
+            onChange={this.onChange}
+          />
+          <Typography className={classes.label}>Password</Typography>
+          {this.state.validationErrors ? 
+            <div style={{ color: "red" }}>
+              {this.state.validationErrors.password}
+            </div>
+          : null}
+          <TextField
+            variant="outlined"
+            margin="normal"
+            fullWidth
+            name="password"
+            type="password"
+            id="password"
+            placeholder="Password"
+            onChange={this.onChange}
+          />
+          <Typography className={classes.label}>Confirm Password</Typography>
+          {this.state.validationErrors ? 
+            <div style={{ color: "red" }}>
+              {this.state.validationErrors.password2}
+            </div>
+          : null}
+          <TextField
+            variant="outlined"
+            margin="normal"
+            fullWidth
+            name="confrimPassword"
+            type="password"
+            id="confrimPassword"
+            placeholder="Confirm Password"
+            onChange={this.onChange}
+          />
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            color="secondary"
+            className={classes.submit}
+          >
+            Sign Up
+          </Button>
+          <Grid container alignItems="center" justify="center" >
+            <Grid item style={{ marginBottom: "50px" }}>
+              <Link href="/login" variant="body2">
+                Already a member? Log In
+              </Link>
+            </Grid>
           </Grid>
-          <Grid item xs={2}></Grid>
-        </Grid>
+        </form>
       </div>
+      </Container>
     );
   }
 }
 
-export default SignUpPage;
+export default withStyles(SignUpStyles)(SignUp);
