7f85408bc971b52107869e7b4e978a636be39150
ykurata <yasukokurata@gmail.com>
2020-01-04 19:08:39 -0500

    update app.js

--- app.js
+++ app.js
@@ -39,11 +39,16 @@ app.use("/conversation", conversationRouter);
 // Set up cors 
 app.use(cors());
 
-// app.use(express.static(path.join(__dirname, "client", "build")))
+// Serve static assets if in production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'));
 
-// app.get("*", (req, res) => {
-//     res.sendFile(path.join(__dirname, "client", "build", "index.html"));
-// });
+  app.get('*', (req, res) => {
+    res.sendFild(path.resolve(__dirname, 'client/build', 'index.html'));
+  });
+}
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
