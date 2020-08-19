79a7e324438a7473308e399f0c636ce6f9232e26
ykurata <yasukokurata@gmail.com>
2020-08-19 16:56:28 -0400

    fixed landing page to responsive

--- client/src/pages/Landing.js
+++ client/src/pages/Landing.js
@@ -1,116 +1,139 @@
-import React, { Component } from "react";
+import React, { useState} from "react";
 
-import Grid from "@material-ui/core/Grid";
-import TextField from "@material-ui/core/TextField";
-import Button from "@material-ui/core/Button";
-
-import "../App.scss";
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import TextField from '@material-ui/core/TextField';
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
 
 import Navbar from "../components/Navbar";
+import landing from "../images/landing-2.png";
 
-class Landing extends Component {
-state = {
-  location: "",
-  dropIn: "",
-  dropOff: ""
-};
+const useStyles = makeStyles((theme) => ({
+  root: {
+    height: '100vh',
+  },
+  image: {
+    backgroundImage: `url(${landing})`,
+    backgroundRepeat: 'no-repeat',
+    backgroundColor:
+      theme.palette.type === 'light' ? theme.palette.grey[50] : theme.palette.grey[900],
+    backgroundSize: 'cover',
+    backgroundPosition: 'center',
+    [theme.breakpoints.down('sm')]: {
+      minHeight: 450
+    },
+  },
+  paper: {
+    margin: theme.spacing(8, 4),
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  form: {
+    padding: '40px',
+    width: '100%', // Fix IE 11 issue.
+    [theme.breakpoints.up('md')]: {
+      marginTop: '50px'
+    },
+  },
+  mainText: {
+    textAlign: 'center',
+    [theme.breakpoints.up('md')]: {
+      textAlign: 'left'
+    },
+  },
+  label: {
+    marginTop: '10px',
+    fontWeight: 'bold',
+    textTransform: 'uppercase'
+  },
+}));
 
-changeLocation = event => {
-  console.log(event.target.value);
-  this.setState({ location: event.target.value });
-};
+const Landing = (props) => {
+  const classes = useStyles();
+  const [userInput, setUserInput] = useState({
+    location: "",
+    dropIn: "",
+    dropOff: ""  
+  });
 
-handleDropInChange = event => {
-  console.log(event.target.value);
-  this.setState({ dropIn: event.target.value });
-};
+  const handleChange = e => {
+    setUserInput({ ...userInput, [e.target.name]: e.target.value });
+  };
+  
+  return (
+    <div>
+      <Navbar/>
+      <Grid container component="main" className={classes.root}>
+        <CssBaseline />
+        <Grid item xs={12} sm={12} md={6} className={classes.image} />
+        <Grid item xs={12} sm={12} md={6} component={Paper} elevation={6} square>
+          <Grid container className={classes.form}>
+            <Grid item xs={12} className={classes.mainText}>
+              <h1>Find the care your dog deserves</h1>
+            </Grid>
 
-handleDropOffChange = event => {
-  console.log(event.target.value);
-  this.setState({ dropOff: event.target.value });
-};
-  render() {
-    return (
-      <div>
-        <Navbar/>
-        <div className="App">
-          <div className="App_aside">
-            <div className="info-box">
-              <Grid container style={{ marginTop: "50px" }}>
-                <Grid item xs={12}>
-                  <h1>Find the care your dog deserves</h1>
-                </Grid>
-
-                <Grid item xs={12}>
-                  <p className="mb-0">
-                    <b>WHERE</b>
-                  </p>
-                </Grid>
-
-                <Grid item xs={10}>
-                  <TextField
-                    id="outlined-bare"
-                    placeholder="Anywhere"
-                    margin="normal"
-                    variant="outlined"
-                    value={this.state.location}
-                    onChange={this.changeLocation}
-                    fullWidth
-                  />
-                </Grid>
-                <Grid item xs={4}></Grid>
-
-                <Grid item xs={12}>
-                  <p className="mb-0">
-                    <b>DROP IN / DROP OFF</b>
-                  </p>
-                </Grid>
-                <Grid item xs={5}>
+            <Grid item xs={12}>
+              <Typography variant="subtitle1" className={classes.label}>
+                Where
+              </Typography>
+              <TextField
+                id="outlined-bare"
+                placeholder="Anywhere"
+                margin="normal"
+                variant="outlined"
+                value={userInput.location}
+                onChange={handleChange}
+                fullWidth
+              />    
+              <Typography variant="subtitle1" className={classes.label}>
+                Drop In/ Drop off
+              </Typography>
+              <Grid container>
+                <Grid item xs={6}>
                   <TextField
                     type="date"
                     name="birthDate"
                     id="standard-birthDate"
-                    value={this.state.dropIn}
-                    onChange={this.handleDropInChange}
+                    value={userInput.dropIn}
+                    onChange={handleChange}
                     margin="normal"
                     variant="outlined"
                     fullWidth
                   />
                 </Grid>
-                <Grid item xs={5}>
+                <Grid item xs={6}>
                   <TextField
                     type="date"
                     name="birthDate"
                     id="standard-birthDate"
-                    value={this.state.dropOff}
-                    onChange={this.handleDropOffChange}
+                    value={userInput.dropOff}
+                    onChange={handleChange}
                     margin="normal"
                     variant="outlined"
                     fullWidth
                   />
                 </Grid>
-                <Grid item xs={4}></Grid>
-
-                <Grid item xs={10}>
-                  <Button
-                    variant="contained"
-                    type="submit"
-                    fullWidth
-                    style={{ backgroundColor: "#f04040", color: "white", marginTop: "30px"}}
-                  >
-                    FIND MY DOG SITTER
-                  </Button>
-                </Grid>
-
-                <Grid item xs={4}></Grid>
               </Grid>
-            </div>
-          </div>
-          <div className="App_form"></div>
-        </div>
-      </div>
-    );
-  }
+              <Button
+                variant="contained"
+                type="submit"
+                fullWidth
+                style={{ backgroundColor: "#f04040", color: "white", marginTop: "30px"}}
+              >
+                FIND MY DOG SITTER
+              </Button>
+            </Grid>
+          
+          </Grid>
+        </Grid>
+       
+      </Grid>
+    </div>
+  );
 }
 
 export default Landing;
