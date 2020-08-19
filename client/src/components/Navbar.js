c70a92d191967b694a6812219784be3d8d263158
ykurata <yasukokurata@gmail.com>
2020-08-19 14:52:20 -0400

    changed collapsing width of navbar

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 import { Link } from "react-router-dom";
 import PropTypes from 'prop-types';
 import {
-  AppBar, Toolbar, Typography, List, ListItem,
-  withStyles, Grid, SwipeableDrawer, Button
+  AppBar, Avatar, Toolbar, Typography, List, ListItem,
+  withStyles, Grid, SwipeableDrawer, IconButton, Button, Menu, MenuItem
 } from '@material-ui/core';
 import MenuIcon from '@material-ui/icons/Menu';
 
@@ -11,7 +12,7 @@ import logo from '../images/loving-sitter-logo.png';
 
 const styleSheet = {
   navbar: {
-    backgroundColor: 'transparent',
+    backgroundColor: 'white',
     boxShadow: "none",
   },
   list : {
@@ -23,7 +24,7 @@ const styleSheet = {
   },
   sideBarIcon : {
     padding : 0,
-    color : "white",
+    color : "black",
     cursor : "pointer",
   },
   menuButton: {
@@ -34,18 +35,40 @@ const styleSheet = {
 class Navbar extends Component{
   constructor(props){
     super(props);
-    this.state = {drawerActivate:false, drawer:false};
+    this.state = {
+      drawerActivate:false,
+      drawer:false,
+      anchorEl: null,
+      setAnchorEl: null,
+      token: localStorage.getItem("jwtToken"),
+      userId: localStorage.getItem("userId"),
+      profile: {}
+    };
     this.createDrawer = this.createDrawer.bind(this);
     this.destroyDrawer = this.destroyDrawer.bind(this);
   }
 
+  handleClick = event => {
+    this.setState({ anchorEl: event.currentTarget });
+  };
+
+  handleClose = () => {
+    this.setState({ anchorEl: null });
+  };
+
+  handleLogout = event => {
+    event.preventDefault();
+    localStorage.clear();
+    window.location.href = "/";
+  };
+
   componentWillMount(){
-    if(window.innerWidth <= 600){
+    if(window.innerWidth <= 800){
       this.setState({drawerActivate:true});
     }
 
     window.addEventListener('resize',()=>{
-      if(window.innerWidth <= 600){
+      if(window.innerWidth <= 800){
         this.setState({drawerActivate:true});
       }
       else{
@@ -54,9 +77,22 @@ class Navbar extends Component{
     });
   }
 
+  componentDidMount() {
+    axios.get(`/profile/get/${this.state.userId}`, { headers: { Authorization: `Bearer ${this.state.token}` } })
+    .then(res => {
+      this.setState({
+          profile: res.data.profile
+      });
+    })
+    .catch(err => {
+        console.log("Error fetching and parsing data", err);
+    });
+  }
+
   //Small Screens
   createDrawer(){
-    const {classes} = this.props
+    const {classes} = this.props;
+    
     return (
       <div>
         <AppBar className={classes.navbar}>
@@ -66,10 +102,10 @@ class Navbar extends Component{
                 className = {this.props.classes.sideBarIcon}
                 onClick={()=>{this.setState({drawer:true})}} />
 
-              <Typography color="inherit" variant = "headline">
+              <Typography color="inherit">
                 <img src={logo} alt="logo"></img>
               </Typography>
-              <Typography color="inherit" variant = "headline"></Typography>
+              <Typography color="inherit"></Typography>
             </Grid>
           </Toolbar>
         </AppBar>
@@ -84,13 +120,23 @@ class Navbar extends Component{
              role="button"
              onClick={()=>{this.setState({drawer:false})}}
              onKeyDown={()=>{this.setState({drawer:false})}}>
-
-            <List className = {this.props.classes.list}>
-               <ListItem key = {1} button divider> Option 1 </ListItem>
-               <ListItem key = {2} button divider> Option 2 </ListItem>
-               <ListItem key = {3} button divider> Option 3 </ListItem>
-             </List>
-
+            {this.state.token ?
+              <List className = {classes.list}>
+                <ListItem button component={Link} to={'/login'}> Become a Sitter </ListItem>
+                <ListItem button component={Link} to={"/sitter-search"}> Find Sitters </ListItem>
+                <ListItem button > Notifications </ListItem>
+                <ListItem button component={Link} to="/messages"> Messages </ListItem>
+                <ListItem button component={Link} to={`/my-profile/${this.state.userId}`}> My Profile</ListItem>
+                <ListItem button component={Link} to="/my-jobs"> My Jobs</ListItem>
+                <ListItem button component={Link} to="/request"> Requests</ListItem>
+                <ListItem button onClick={this.handleLogout}>Log Out</ListItem>
+              </List>
+            : <List className = {this.props.classes.list}>
+                <ListItem button>Find Sitters</ListItem>
+                <ListItem button component={Link} to={'/login'}>Log In</ListItem>
+                <ListItem button component={Link} to={'/signup'}> Sign Up</ListItem>
+              </List>
+            }
          </div>
        </SwipeableDrawer>
 
@@ -100,38 +146,83 @@ class Navbar extends Component{
 
   //Larger Screens
   destroyDrawer(){
-    const {classes} = this.props
+    const {classes} = this.props;
+    const { anchorEl } = this.state;
+    const open = Boolean(anchorEl);
     return (
       <AppBar className={classes.navbar}>
         <Toolbar>
-          <Typography variant = "headline" style={{flexGrow:1}} color="inherit" >
+          <Typography style={{flexGrow:1}} color="inherit" >
             <img src={logo} alt="logo"></img>
           </Typography>
-          <Button
-            className={classes.menuButton}
-            color="inherit"
-            component={Link}
-            to={"/profile"}
-          >
-            BECOME A SITTER
-          </Button>
-          <Button
-            className={classes.menuButton}
-            variant="outlined"
-            color="secondary"
-            component={Link}
-            to={"/login"}
-          >
-            Log In
-          </Button>
-          <Button
-            variant="contained"
-            color="secondary"
-            component={Link}
-            to={"/signup"}
-          >
-            Sign Up
-          </Button>
+          {this.state.token ? 
+            <span>
+              <Button component={Link} to={"/profile"}>
+                BECOME A SITTER
+              </Button>
+              <Button component={Link} to={"/sitter-search"}>
+                Find Sitters
+              </Button>
+              <Button >
+                Notifications
+              </Button>
+              <Button component={Link} to={"/messages"}>
+                Messages
+              </Button>
+              <IconButton aria-label="avatar" onClick={this.handleClick}>
+                <Avatar
+                  alt="Remy Sharp"
+                  src=""
+                  className={classes.avatar}
+                />
+              </IconButton>
+              <Menu
+                id="menu-appbar"
+                anchorEl={anchorEl}
+                keepMounted
+                open={open}
+                onClose={this.handleClose}
+              >
+                <MenuItem component={Link} to={`/my-profile/${this.state.userId}`}>
+                  My Profile
+                </MenuItem>
+                <MenuItem component={Link} to={"/my-jobs"}>
+                  My Jobs
+                </MenuItem>
+                <MenuItem component={Link} to={"/request"}>
+                  Requests
+                </MenuItem>
+                <MenuItem onClick={this.handleLogout}>Log Out</MenuItem>
+              </Menu>
+            </span>
+          : <span>
+              <Button
+                className={classes.menuButton}
+                component={Link}
+                to={"/profile"}
+              >
+                BECOME A SITTER
+              </Button>
+              <Button
+                className={classes.menuButton}
+                variant="outlined"
+                color="secondary"
+                component={Link}
+                to={"/login"}
+              >
+                Log In
+              </Button>
+              <Button
+                variant="contained"
+                color="secondary"
+                component={Link}
+                to={"/signup"}
+              >
+                Sign Up
+              </Button>
+            </span>
+          }
+          
         </Toolbar>
       </AppBar>
     )
