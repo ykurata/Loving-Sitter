d4056d475fdf2eac9cad989c9e353d3331a76b80
ykurata <yasukokurata@gmail.com>
2020-08-20 15:38:24 -0400

    updated my profle page

--- client/src/pages/MyProfile.js
+++ client/src/pages/MyProfile.js
@@ -1,21 +1,20 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import axios from "axios";
 
-import Grid from "@material-ui/core/Grid";
 import Avatar from "@material-ui/core/Avatar";
 import Box from "@material-ui/core/Box";
 import Button from "@material-ui/core/Button";
+import Grid from "@material-ui/core/Grid";
 import Typography from "@material-ui/core/Typography";
-import { withStyles } from "@material-ui/core/styles";
+import { makeStyles } from "@material-ui/core/styles";
 import RoomIcon from "@material-ui/icons/Room";
 import AccountCircleIcon from '@material-ui/icons/AccountCircle';
 import { Snackbar, IconButton } from "@material-ui/core";
 
 import Navbar from "../components/Navbar";
 
-
-const MyProfileStyle = theme => ({
+const MyProfileStyle = makeStyles(theme => ({
   avatar: {
     width: 300,
     height: 300,
@@ -32,119 +31,112 @@ const MyProfileStyle = theme => ({
     width: 300,
     height: 300
   }
-});
+}));
 
-class MyProfile extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      profile: {},
-      token: localStorage.getItem("jwtToken"),
-      snackbaropen: false,
-      snackbarmsg: ""  
-    }
+const MyProfile = (props) => {
+  const classes = MyProfileStyle();
+  const [profile, setProfile] = useState({});
+  const token = localStorage.getItem("jwtToken");
+  const userId = localStorage.getItem("userId");
+  const [snackbarOpen, setSnackbarOpen] = useState(false);
+  const [snackbarMsg, setSnackbarMsg] = useState("");
+  
+  useEffect(() => {
+    axios.get(`/profile/get/${userId}`, {
+      headers: { Authorization: `Bearer ${token}` }
+    })
+    .then(res => {
+      setProfile(res.data.profile);
+    })
+    .catch(err => {
+      console.log("Error fetching and parsing data", err);
+    });
+  }, [])
+
+  const snackbarClose = e => {
+    setSnackbarOpen(false);
   };
 
-  componentDidMount() {
-    axios.get(`/profile/get/${this.props.match.params.id}`, {
-        headers: { Authorization: `Bearer ${this.state.token}` }
-      })
-      .then(res => {
-        this.setState({
-          profile: res.data.profile,
-        });
-      })
-      .catch(err => {
-        console.log("Error fetching and parsing data", err);
-      });
-  }
+  console.log(profile)
 
-  snackbarClose = event => {
-    this.setState({ snackbaropen: false });
-  };
+  return (
+    <div>
+      <Snackbar
+        anchorOrigin={{
+          vertical: "bottom",
+          horizontal: "left"
+        }}
+        open={snackbarOpen}
+        autoHideDuration={3000}
+        onClose={snackbarClose}
+        message={<span id="message-id">{snackbarMsg}</span>}
+        action={[
+          <IconButton
+            key="close"
+            arial-label="Close"
+            color="inherit"
+            onClick={snackbarClose}
+          ></IconButton>
+        ]}
+      />
 
-  render() {
-    const { classes } = this.props;
-    const { profile } = this.state;
-    return (
-      <div>
-        <Snackbar
-          anchorOrigin={{
-            vertical: "bottom",
-            horizontal: "left"
-          }}
-          open={this.state.snackbaropen}
-          autoHideDuration={3000}
-          onClose={this.snackbarClose}
-          message={<span id="message-id">{this.state.snackbarmsg}</span>}
-          action={[
-            <IconButton
-              key="close"
-              arial-label="Close"
-              color="inherit"
-              onClick={this.snackbarClose}
-            ></IconButton>
-          ]}
-        />
-
-        <Navbar/>
-        <Grid container justify="center" style={{ marginTop: "50px"}}>
-          <Grid item xs={7}>
-            <Grid container align="center">
-              <Box width={1} boxShadow={2} style={{marginBottom: "30px"}}>
-                <Grid item>
-                  {profile.photoUrl ?
-                    <Avatar
-                      alt="Your Profile Picture"
-                      src={profile.photoUrl}
-                      className={classes.avatar}
-                    />
-                  : <AccountCircleIcon className={classes.avatar} color="disabled"/> 
-                  }
-                </Grid>
-                <Grid item>
-                  <Typography variant="h4">
-                    {profile.firstName} {profile.lastName}
+      <Navbar/>
+      <Grid container justify="center" style={{ marginTop: "50px"}}>
+        <Grid item xs={7}>
+          <Grid container align="center">
+            <Box width={1} boxShadow={2} style={{marginBottom: "30px"}}>
+              <Grid item>
+                {profile.photoUrl ?
+                  <Avatar
+                    alt="Your Profile Picture"
+                    src={profile.photoUrl}
+                    className={classes.avatar}
+                  />
+                : <AccountCircleIcon className={classes.avatar} color="disabled"/> 
+                }
+              </Grid>
+              <Grid item>
+                <Typography variant="h4">
+                  {profile.firstName} {profile.lastName}
+                </Typography>
+              </Grid>
+              <Grid container justify="center" style={{marginTop: "30px"}}> 
+                <RoomIcon  color="secondary" />
+                <Typography variant="subtitle1">{profile.address}</Typography>
+                <Grid item style={{ marginLeft: "100px"}}>
+                  <Typography variant="subtitle1">
+                    <Box fontWeight="fontWeightBold">
+                      $ {profile.rate}/hr
+                    </Box>
                   </Typography>
                 </Grid>
-                <Grid container justify="center" style={{marginTop: "30px"}}> 
-                  <RoomIcon  color="secondary" />
-                  <Typography variant="subtitle1">{profile.address}</Typography>
-                  <Grid item style={{ marginLeft: "100px"}}>
-                    <Typography variant="subtitle1">
-                      <Box fontWeight="fontWeightBold">
-                        $ {profile.rate}/hr
-                      </Box>
-                    </Typography>
-                  </Grid>
-                </Grid>
-                <Grid
-                  item
-                  xs={11}
-                  align="left"
-                  style={{marginTop: "30px"}}
+              </Grid>
+              <Grid
+                item
+                xs={11}
+                align="left"
+                style={{marginTop: "30px"}}
+              > 
+                <Typography
+                  className={classes.marginHorizontal}
+                  variant="body1"
+                  component="div"
                 > 
-                  <Typography
-                    className={classes.marginHorizontal}
-                    variant="body1"
-                    component="div"
-                  > 
-                    <Box fontWeight="fontWeightBold" fontSize="h6.fontSize">
-                      About Me
-                    </Box><br></br>
-                    {profile.description}
-                  </Typography>
-                </Grid>
-                <Grid item style={{marginBottom: "30px"}} >
-                  <Button variant="outlined" color="primary" component={Link} to={'/profile'}>Edit Profile</Button>
-                </Grid>
-              </Box>
-            </Grid>
+                  <Box fontWeight="fontWeightBold" fontSize="h6.fontSize">
+                    About Me
+                  </Box><br></br>
+                  {profile.description}
+                </Typography>
+              </Grid>
+              <Grid item style={{marginBottom: "30px"}} >
+                <Button variant="outlined" color="primary" component={Link} to={'/profile'}>Edit Profile</Button>
+              </Grid>
+            </Box>
           </Grid>
         </Grid>
-      </div>
-    );
-  }
+      </Grid>
+    </div>
+  );
 }
 
-export default withStyles(MyProfileStyle)(MyProfile);
+export default MyProfile;
