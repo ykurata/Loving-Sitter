f40a1c8589a913d5f43a1af34593f3738b40907f
ykurata <yasukokurata@gmail.com>
2020-08-28 11:40:27 -0400

    set up redux

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,8 @@
 import React from "react";
+import setAuthToken from "./utils/setAuthToken";
+import { setCurrentUser } from "./actions/authActions";
+import { Provider } from "react-redux";
+
 import { BrowserRouter, Route } from "react-router-dom";
 import { MuiThemeProvider } from "@material-ui/core";
 import { theme } from "./themes/theme";
@@ -18,24 +22,41 @@ import ProfileDetails from "./pages/ProfileDetails";
 import Messages from "./pages/Messages";
 import PrivateRoute from "./components/PrivateRoute";
 
+import store from "./store";
+
+if (localStorage.jwtToken) {
+  // Set auth token header auth
+  const token = localStorage.jwtToken;
+  setAuthToken(token);
+  // Decode token and get user info and exp
+  const decoded = jwt_decode(token);
+  // Set user and isAuthenticated
+  store.dispatch(setCurrentUser(decoded));
+}
+
 function App() {
   return (
-    <MuiThemeProvider theme={theme}>
-      <BrowserRouter>
-        <PrivateRoute path='/profile' component={EditProfile} />
-        <PrivateRoute path="/profile-details/:id" component={ProfileDetails} />
-        <PrivateRoute path="/my-profile/:id" component={MyProfile} />
-        <PrivateRoute path="/sitter-search" component={ProfileList} />
-        <PrivateRoute path="/messages" component={Messages} />
-        <PrivateRoute path="/payment" component={Payment} />
-        <PrivateRoute path="/profile-photo" component={Photo} />
-        <PrivateRoute path="/my-jobs" component={MyJobs} />
-        <PrivateRoute path="/request" component={Request} />
-        <Route exact path="/" component={Landing} />
-        <Route path="/login" component={Login} />
-        <Route path="/signup" component={SignUp} />
-      </BrowserRouter>
-    </MuiThemeProvider>
+    <Provider store={store}>
+      <MuiThemeProvider theme={theme}>
+        <BrowserRouter>
+          <PrivateRoute path="/profile" component={EditProfile} />{" "}
+          <PrivateRoute
+            path="/profile-details/:id"
+            component={ProfileDetails}
+          />
+          <PrivateRoute path="/my-profile/:id" component={MyProfile} />{" "}
+          <PrivateRoute path="/sitter-search" component={ProfileList} />{" "}
+          <PrivateRoute path="/messages" component={Messages} />{" "}
+          <PrivateRoute path="/payment" component={Payment} />{" "}
+          <PrivateRoute path="/profile-photo" component={Photo} />{" "}
+          <PrivateRoute path="/my-jobs" component={MyJobs} />{" "}
+          <PrivateRoute path="/request" component={Request} />{" "}
+          <Route exact path="/" component={Landing} />{" "}
+          <Route path="/login" component={Login} />{" "}
+          <Route path="/signup" component={SignUp} />{" "}
+        </BrowserRouter>{" "}
+      </MuiThemeProvider>
+    </Provider>
   );
 }
 
