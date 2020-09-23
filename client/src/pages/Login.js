f5582b2964fb8856a782b4cdde674092ac97d7f2
ykurata <yasukokurata@gmail.com>
2020-09-23 16:24:45 -0400

    updated login page UI

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,140 +1,123 @@
 import React, { useState, useEffect } from "react";
-import { Link } from "react-router-dom";
-import { useSelector, useDispatch } from "react-redux";
-import { loginUser } from "../actions/authActions";
-
+import Avatar from "@material-ui/core/Avatar";
 import Button from "@material-ui/core/Button";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import TextField from "@material-ui/core/TextField";
+import Link from "@material-ui/core/Link";
+import Paper from "@material-ui/core/Paper";
 import Grid from "@material-ui/core/Grid";
+import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
 import Typography from "@material-ui/core/Typography";
 import { makeStyles } from "@material-ui/core/styles";
-import Container from "@material-ui/core/Container";
 
-const loginStyles = makeStyles((theme) => ({
+import login from "../images/login.jpg";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    height: "100vh",
+  },
+  image: {
+    backgroundImage: `url(${login})`,
+    backgroundRepeat: "no-repeat",
+    backgroundColor:
+      theme.palette.type === "light"
+        ? theme.palette.grey[50]
+        : theme.palette.grey[900],
+    backgroundSize: "cover",
+    backgroundPosition: "center",
+  },
   paper: {
-    marginTop: theme.spacing(18),
+    margin: theme.spacing(8, 8),
     display: "flex",
     flexDirection: "column",
     alignItems: "center",
+    [theme.breakpoints.down("sm")]: {
+      margin: theme.spacing(8, 4),
+    },
   },
-  label: {
-    margin: theme.spacing(1, 1, 1, 0),
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.main,
   },
   form: {
     width: "100%", // Fix IE 11 issue.
     marginTop: theme.spacing(1),
   },
   submit: {
-    margin: theme.spacing(3, 0, 2),
-    marginRight: 10,
+    margin: theme.spacing(3, 2, 2),
   },
-  error: {
-    color: "red",
+  buttonContainer: {
+    textAlign: "center",
   },
 }));
 
-const Login = (props) => {
-  const classes = loginStyles();
-  const [userInput, setUserInput] = useState({
-    email: "",
-    password: "",
-  });
-  const dispatch = useDispatch();
-  const auth = useSelector((state) => state.auth);
-  const error = useSelector((state) => state.error);
-
-  // Update user input
-  const onChange = (e) => {
-    setUserInput({ ...userInput, [e.target.name]: e.target.value });
-  };
-
-  useEffect(() => {
-    if (auth.isAuthenticated) {
-      props.history.push("/sitter-search");
-    }
-  });
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    dispatch(loginUser(userInput));
-  };
-
-  const demoLogin = (e) => {
-    e.preventDefault();
-    const demoUser = {
-      email: "demouser@email.com",
-      password: "password",
-    };
-    dispatch(loginUser(demoUser));
-  };
+const Login = () => {
+  const classes = useStyles();
 
   return (
-    <Container component="main" maxWidth="xs">
+    <Grid container component="main" className={classes.root}>
       <CssBaseline />
-      <div className={classes.paper}>
-        <Typography component="h1" variant="h4">
-          Log In
-        </Typography>
-        {error.error ? (
-          <div className={classes.error}>{error.error}</div>
-        ) : null}
-        <form className={classes.form} onSubmit={handleSubmit}>
-          <Typography className={classes.label}>Email Address</Typography>
-          {error ? <div className={classes.error}>{error.email}</div> : null}
-          <TextField
-            variant="outlined"
-            margin="normal"
-            fullWidth
-            id="email"
-            name="email"
-            placeholder="Email Address"
-            onChange={onChange}
-          />
-          <Typography className={classes.label}>Password</Typography>
-          {error ? <div className={classes.error}>{error.password}</div> : null}
-          <TextField
-            variant="outlined"
-            margin="normal"
-            fullWidth
-            name="password"
-            type="password"
-            id="password"
-            placeholder="Password"
-            onChange={onChange}
-          />
-          <Button
-            type="submit"
-            variant="contained"
-            color="secondary"
-            className={classes.submit}
-          >
-            Log In
-          </Button>
-          <Button
-            type="submit"
-            variant="contained"
-            color="default"
-            className={classes.submit}
-            onClick={demoLogin}
-          >
-            Demo User
-          </Button>
-          <Grid container alignItems="center" justify="center">
-            <Grid item>
-              <Typography
-                variant="body2"
-                component={Link}
-                to={"/signup"}
-                style={{ textDecoration: "none" }}
+      <Grid item xs={false} sm={4} md={6} className={classes.image} />
+      <Grid item xs={12} sm={8} md={6} component={Paper} elevation={6} square>
+        <div className={classes.paper}>
+          <Avatar className={classes.avatar}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Sign in
+          </Typography>
+          <form className={classes.form} noValidate>
+            <TextField
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              id="email"
+              label="Email Address"
+              name="email"
+              autoComplete="email"
+              autoFocus
+            />
+            <TextField
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              name="password"
+              label="Password"
+              type="password"
+              id="password"
+              autoComplete="current-password"
+            />
+            <div className={classes.buttonContainer}>
+              <Button
+                type="submit"
+                size="large"
+                variant="contained"
+                color="secondary"
+                className={classes.submit}
               >
-                Don't have an account? Sign Up
-              </Typography>
-            </Grid>
-          </Grid>
-        </form>
-      </div>
-    </Container>
+                Sign In
+              </Button>
+              <Button
+                type="submit"
+                size="large"
+                variant="outlined"
+                color="secondary"
+                className={classes.submit}
+              >
+                Demo User
+              </Button>
+            </div>
+            <div className={classes.buttonContainer}>
+              <Link href="/signup" variant="body2">
+                {"Don't have an account? Sign Up"}
+              </Link>
+            </div>
+          </form>
+        </div>
+      </Grid>
+    </Grid>
   );
 };
 
